793768dc84c529dea6197a093b272748381a3adc
dstarchenko26-prog <denys.starchenko.oi.2023@lpnu.ua>
2026-09-13 13:04:03 +0300

    issue 2
    Ініціалізація Spring проєкту

---
+++ src/main/java/ua/lpnu/practBack/PractBackApplication.java
@@ -0,0 +1,13 @@
+package ua.lpnu.practBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/lpnu/practBack/PractBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lpnu.practBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
